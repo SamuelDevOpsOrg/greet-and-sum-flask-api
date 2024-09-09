a53c82d781cd02fd547befad7eb291f324fb2b06
Samuel Oluwade <samueloluwade.devops@gmail.com>
2024-09-09 00:58:39 -0500

    updated app.py for production

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -20,4 +21,5 @@ def add():
     return jsonify({"result": a + b})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+# app.run(debug=True)
