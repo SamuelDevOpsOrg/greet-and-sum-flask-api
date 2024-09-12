20841d92a0b9d7e136d4ef6480a5a164b69f8f53
Samuel Oluwade <samueloluwade.devops@gmail.com>
2024-09-12 00:28:32 -0500

    added install pylint and pytest

--- test_app.py
+++ test_app.py
@@ -12,7 +12,6 @@ def test_hello(client):
     """Test the hello endpoint."""
     response = client.get('/')
     json_data = response.get_json()
-    
     assert response.status_code == 200
     assert json_data['message'] == 'Hello, World!!'
 
@@ -20,7 +19,6 @@ def test_greet(client):
     """Test the greet endpoint."""
     response = client.get('/greet/Samuel')
     json_data = response.get_json()
-
     assert response.status_code == 200
     assert json_data['message'] == 'Hello, Samuel!'
 
@@ -28,7 +26,6 @@ def test_add(client):
     """Test the add endpoint with POST request."""
     response = client.post('/add', json={'a': 2, 'b': 3})
     json_data = response.get_json()
-    
     assert response.status_code == 200
     assert json_data['result'] == 5
 
@@ -36,5 +33,4 @@ def test_add(client):
 #     """Test the add endpoint with missing parameters."""
 #     response = client.post('/add', json={'a': 2})
 #     json_data = response.get_json()
-        
 #     assert response.status_code == 500  # Should return a 500 error due to missing 'b'
